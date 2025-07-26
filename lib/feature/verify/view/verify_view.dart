cc41f74a4cbac439c428fb9bbc840b8e76e5ed13
Pravasta Rama Fitrayana <fitrayanaf15@gmail.com>
2025-07-26 14:47:25 +0700

    update fix

--- lib/feature/verify/view/verify_view.dart
+++ lib/feature/verify/view/verify_view.dart
@@ -1,5 +1,10 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_credit_management/core/components/app_button.dart';
+import 'package:flutter_credit_management/core/style/custom_shadow.dart';
+import 'package:flutter_credit_management/feature/verify/view/widgets/custom_progress_bar.dart';
 import 'package:flutter_credit_management/main.dart';
+
+import '../../../core/utils/assets.gen.dart';
 
 class VerifyView extends StatefulWidget {
   const VerifyView({super.key});
@@ -11,12 +16,152 @@ class VerifyView extends StatefulWidget {
 }
 
 class _VerifyViewState extends State<VerifyView> {
+  List<String> verification = [];
+
+  @override
+  void initState() {
+    super.initState();
+    verification = ['Income Verification', 'National ID Verification'];
+  }
+
+  Widget appBar() {
+    return Container(
+      width: double.infinity,
+      margin: EdgeInsets.fromLTRB(24, 20, 24, 0),
+      child: Row(
+        children: [
+          Image.asset(
+            Assets.icons.logo.path,
+            width: 32,
+            height: 32,
+            fit: BoxFit.cover,
+          ),
+          SizedBox(width: 13),
+          Expanded(
+            child: Text(
+              'CrediChain',
+              style: appTextTheme(context).bodyLarge!.copyWith(
+                fontWeight: FontWeight.w400,
+                color: Color(0xff02275A),
+              ),
+            ),
+          ),
+          Image.asset(
+            Assets.icons.mail.path,
+            width: 24,
+            height: 24,
+            fit: BoxFit.cover,
+          ),
+        ],
+      ),
+    );
+  }
+
+  Widget contentSection() {
+    return Expanded(
+      child: Container(
+        width: double.infinity,
+        padding: EdgeInsets.symmetric(horizontal: 20),
+        child: SingleChildScrollView(
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              SizedBox(height: 20),
+              Text(
+                'Identify Verification',
+                style: appTextTheme(context).titleMedium!.copyWith(
+                  fontWeight: FontWeight.w600,
+                  color: Color(0xff02275A),
+                ),
+              ),
+              SizedBox(height: 30),
+              Container(
+                padding: EdgeInsets.all(20),
+                decoration: BoxDecoration(
+                  color: Color(0xfffffcfc),
+                  borderRadius: BorderRadius.circular(20),
+                  boxShadow: getCustomBoxShadow(),
+                ),
+                child: Column(
+                  children: [
+                    Container(
+                      width: 120,
+                      height: 120,
+                      decoration: BoxDecoration(
+                        color: Color(0xfff6f6f6),
+                        border: Border.all(width: 1, color: Color(0xffeaeaea)),
+                        shape: BoxShape.circle,
+                      ),
+                      child: Center(
+                        child: Text(
+                          'MM',
+                          style: appTextTheme(context).titleLarge!.copyWith(
+                            fontWeight: FontWeight.w600,
+                            color: Color(0xff02275A),
+                          ),
+                        ),
+                      ),
+                    ),
+                    SizedBox(height: 20),
+                    CustomProgressBar(progress: 0.75),
+                    SizedBox(height: 20),
+                    Column(
+                      children:
+                          [
+                            'Income Verification',
+                            'National ID Verification',
+                            'Face Verification',
+                            'SSI Verification',
+                          ].map((e) {
+                            return Row(
+                              children: [
+                                Checkbox(
+                                  value: verification.contains(e),
+                                  onChanged: (value) {
+                                    setState(() {
+                                      if (value == true) {
+                                        verification.add(e);
+                                      } else {
+                                        verification.remove(e);
+                                      }
+                                    });
+                                  },
+                                ),
+                                Text(
+                                  e,
+                                  style: appTextTheme(
+                                    context,
+                                  ).bodyMedium!.copyWith(
+                                    fontWeight: FontWeight.w500,
+                                    color: Color(0xff02275A),
+                                  ),
+                                ),
+                              ],
+                            );
+                          }).toList(),
+                    ),
+                    SizedBox(height: 20),
+                    DefaultButton(
+                      title: 'Continue Verification',
+                      onTap: () {},
+                      backgroundColor: Color(0xff005BA4),
+                      borderColor: Color(0xff005BA4),
+                      titleColor: Colors.white,
+                    ),
+                  ],
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: Center(
-        child: Text('Verify View', style: appTextTheme(context).headlineMedium),
-      ),
+    return SafeArea(
+      child: Scaffold(body: Column(children: [appBar(), contentSection()])),
     );
   }
 }
---
+++ lib/feature/verify/view/widgets/custom_progress_bar.dart
@@ -0,0 +1,85 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_credit_management/main.dart';
+
+class CustomProgressBar extends StatefulWidget {
+  final double progress; // e.g. 0.75
+
+  const CustomProgressBar({super.key, required this.progress});
+
+  @override
+  State<CustomProgressBar> createState() => _CustomProgressBarState();
+}
+
+class _CustomProgressBarState extends State<CustomProgressBar>
+    with SingleTickerProviderStateMixin {
+  late AnimationController _controller;
+  late Animation<double> _animation;
+
+  @override
+  void initState() {
+    super.initState();
+    _controller = AnimationController(
+      duration: const Duration(milliseconds: 500),
+      vsync: this,
+    );
+
+    _animation = Tween<double>(
+      begin: 0.0,
+      end: widget.progress,
+    ).animate(CurvedAnimation(parent: _controller, curve: Curves.easeInOut));
+
+    _controller.forward();
+  }
+
+  @override
+  void dispose() {
+    _controller.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return AnimatedBuilder(
+      animation: _animation,
+      builder: (context, child) {
+        return Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Stack(
+              children: [
+                // Background bar
+                Container(
+                  height: 12,
+                  decoration: BoxDecoration(
+                    color: Colors.grey[200],
+                    borderRadius: BorderRadius.circular(10),
+                  ),
+                ),
+                // Foreground progress bar
+                FractionallySizedBox(
+                  alignment: Alignment.centerLeft,
+                  widthFactor: _animation.value,
+                  child: Container(
+                    height: 12,
+                    decoration: BoxDecoration(
+                      color: Color(0xcc005BA4),
+                      borderRadius: BorderRadius.circular(10),
+                    ),
+                  ),
+                ),
+              ],
+            ),
+            const SizedBox(height: 8),
+            Text(
+              "${(_animation.value * 100).round()}%",
+              style: appTextTheme(context).bodySmall!.copyWith(
+                color: Color(0xcc005BA4),
+                fontWeight: FontWeight.w500,
+              ),
+            ),
+          ],
+        );
+      },
+    );
+  }
+}
